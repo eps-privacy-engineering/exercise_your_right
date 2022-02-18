408acb5e476e495e021626eb7be2357e971546fb
joywang0 <joy_wang@sjtu.edu.cn>
2022-02-18 16:47:03 -0500

    click

--- background.js
+++ background.js
@@ -8,16 +8,3 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 	{urls: ['<all_urls>']},
 	['blocking', 'requestHeaders', 'extraHeaders']
 );
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-console.log("CALL GET~s2");
-console.log(httpGet("https://www.xfinity.com/privacy/manage-preference"));
-console.log("CALL GET~e2");
-
-
--- document_start.js
+++ document_start.js
@@ -15,3 +15,28 @@ function httpGet(theUrl)
 console.log(httpGet("https://www.xfinity.com/privacy/manage-preference"));
 console.log("CALL GET");
 
+//console.log("Hi3");
+//if (window.location.href!="https://www.xfinity.com/privacy/manage-preference"){
+//    window.location="https://www.xfinity.com/privacy/manage-preference";
+//}
+//console.log("Hi4");
+
+setTimeout(function(){
+    var elems=document.body.getElementsByTagName("*");//Do Not Sell My Personal Information
+    console.log("Hi5",elems);
+    for(var i=0;i<elems.length;i++){
+        console.log("tag6",elems[i].textContent);
+        console.log("tag7",elems[i].innerText);
+        console.log("tag8",elems[i].href);
+    }
+}, 2000);
+
+//document.getElementById('digital-footer-bottom-link-bottom-9').click();
+
+//<a class="xc-footer--terms-link" href="https://www.xfinity.com/privacy/manage-preference" id="xc-footer--terms">Do Not Sell My Personal Information</a>
+
+//document.getElementById('xc-footer--terms').click();
+
+
+
+
