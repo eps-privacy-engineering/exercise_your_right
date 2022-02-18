2aa1c80605e12fc6ea65411d766b684b676e7e6e
joywang0 <joy_wang@sjtu.edu.cn>
2022-02-18 16:01:33 -0500

    update httpget

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+console.log("Hi2");
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	function(details) {
 		details.requestHeaders.push({name: 'Sec-GPC', value: '1'});
@@ -6,3 +8,16 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 	{urls: ['<all_urls>']},
 	['blocking', 'requestHeaders', 'extraHeaders']
 );
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+console.log("CALL GET~s2");
+console.log(httpGet("https://www.xfinity.com/privacy/manage-preference"));
+console.log("CALL GET~e2");
+
+
--- document_start.js
+++ document_start.js
@@ -1,4 +1,17 @@
+console.log("Hi");
 const script = document.createElement('script');
 script.innerHTML = 'Object.defineProperty(navigator, \'globalPrivacyControl\', {get: () => true, set: (v) => {}});';
 (document.head || document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+console.log(httpGet("https://www.xfinity.com/privacy/manage-preference"));
+console.log("CALL GET");
+
