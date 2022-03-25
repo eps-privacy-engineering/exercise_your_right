226ef60a6cce99a3bd3e5057b2573342d8427733
joywang0 <joy_wang@sjtu.edu.cn>
2022-03-24 20:43:24 -0400

    halfway - manifest version 3

---
+++ db_backend.js
@@ -0,0 +1,21 @@
+// "www.xfinity.com": {
+//  "ccpa_do_not_sell": {
+//   "right_type": "CCPA_Sell",
+//   "exercise_path": [
+//    {
+//     "text": "Do Not Sell My Personal Information",
+//     "category": "href",
+//     "operation_type": "click"
+//    }
+//   ]
+//  },
+//  "ccpa_delete": null,
+//  "ccpa_copy": null
+// }
+
+if (get_website_attr(Host)){
+  // pop_up window delete button link with database
+}
+else{
+  // go to website to get attr
+}
